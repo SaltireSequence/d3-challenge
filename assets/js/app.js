7b81ddadb5515f3c52ddc8c8fffa3d49bc242a33
William F <60886603+wtcf90@users.noreply.github.com>
2020-08-03 18:16:38 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,32 +2,39 @@
 function chatRender() {
 
 // Calling chart, if chartSVG is not empty, when brower loads index
-var chartSVG = d3.select("body").select("svg");
-// Clear chartSVG if empty
-if (!chartSVG.empty()) {
-  chartSVG.remove();
-}
+  var chartSVG = d3.select("body").select("svg");
+  // Clear chartSVG if empty
+  if (!chartSVG.empty()) {
+    chartSVG.remove();
+  }
 
-// Using a proportional scaler, to set chart dimensions.
-var chartWidth = 1000;
-var chartHeight = 612.24
+  // Using a proportional scaler, to set chart dimensions.
+  var chartWidth = 1000;
+  var chartHeight = 612.24
 
-// Setting appropriate margins
-var margins = {
-  top: 30,
-  right: 40,
-  bottom: 120,
-  left: 100
-};
+  // Setting appropriate margins
+  var margins = {
+    top: 30,
+    right: 40,
+    bottom: 120,
+    left: 100
+  };
 
-/* Defining dimensions of chart
-REF: https://observablehq.com/@d3/margin-convention */
-var height = chartWidth - margin.top - margin.bottom;
-var width = chartHeight - margin.left - margin.right;
+  /* Defining dimensions of chart
+  REF: https://observablehq.com/@d3/margin-convention */
+  var height = chartWidth - margin.top - margin.bottom;
+  var width = chartHeight - margin.left - margin.right;
 
-/* SVG wrapper to hold the chart. I appended my SVG group, that will
-hold my chart*/ 
-var svgWrapper = d3.select("#scatter")
-.append("svg")
-  .attr("width", chartWidth)
-  .attr("height", chartHeight)
+  /* SVG wrapper to hold the chart. I appended my SVG group, that will
+  hold my chart
+  REF: https://stackoverflow.com/questions/55988709/how-can-i-add-labels
+  -inside-the-points-in-a-scatterplot*/
+  var svgWrapper = d3.select("#scatter")
+  .append("svg")
+    .attr("width", chartWidth)
+    .attr("height", chartHeight)
+
+  var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  var
