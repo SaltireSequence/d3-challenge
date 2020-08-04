42a8b56505daa386ef57dfda7a5d03000344e6f1
William F <60886603+wtcf90@users.noreply.github.com>
2020-08-03 17:47:48 -0700

    calling chart and setting parameters

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,15 @@
 // @TODO: YOUR CODE HERE!
 function chatRender() {
 
+// Calling chart, if chartSVG is not empty, when brower loads index
 var chartSVG = d3.select("body").select("svg");
+// Clear chartSVG if empty
 if (!chartSVG.empty()) {
   chartSVG.remove();
 }
 
-var chartWidth =
+// Using a proportional scaler, to set chart dimensions.
+var chartWidth = 1000;
+var chartHeight = 612.24
+
+var margins = {}
